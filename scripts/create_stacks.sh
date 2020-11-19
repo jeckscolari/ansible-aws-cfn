c3106bec3be34e51cbbc3252e0bac186ee2ec1ca
jeckscolari <giacomo.scolari92@gmail.com>
2020-11-19 21:10:13 +0100

    fix create stack script

--- scripts/create_stacks.sh
+++ scripts/create_stacks.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 for filename in cfn_templates/*.yml; do
-    stack = $(basename "$filename" .yml)
+    stack=$(basename "$filename" .yml)
+    filename=$(basename "$filename")
 
     aws cloudformation create-stack \
         --stack-name $stack \
-        s3://cfn-templates-demo/filename
+        --template-url s3://cfn-templates-demo/$filename
+
 done
 
  
