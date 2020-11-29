cf21b2b72a1ad765815d1e901f38dc78c0c0e652
jeckscolari <giacomo.scolari92@gmail.com>
2020-11-29 15:06:05 +0100

    provision ec2 instances and alb

---
+++ parameter_store.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+KEY_PATH=".ssh/ansible.pem"
+PARAMETER_PATH="/ansible-cfn"
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/ansible_private_key \
+  --type SecureString \
+  --value "file://${KEY_PATH}" \
+  --description "Ansible private key for EC2 instances" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/ssh_location \
+  --type String \
+  --value "0.0.0.0/0" \
+  --description "IP Address Range from which SSH is allowed" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/key_name \
+  --type String \
+  --value "ansible" \
+  --description "SSH key name" \
+  --overwrite
+
+aws ssm get-parameter \
+  --with-decryption \
+  --name $PARAMETER_PATH/ansible_private_key \
+  --query Parameter.Value
