ee545c77e90133b967f245667a9b0e6ff0f068b5
jeckscolari <giacomo.scolari92@gmail.com>
2020-12-12 13:24:19 +0100

    remove unnecessary parameters for cfn stack

--- scripts/get_ssh_key.sh
+++ scripts/get_ssh_key.sh
@@ -2,12 +2,12 @@
 
 PARAMETER_PATH="/ansible-cfn"
 
-mkdir -p ~/.ssh
+mkdir .ssh
 
 aws ssm get-parameter \
   --with-decryption \
   --name $PARAMETER_PATH/ansible_private_key \
   --query Parameter.Value \
-  --output text > ~/.ssh/ansible
+  --output text > .ssh/ansible
 
-chmod 600 ~/.ssh/ansible
+chmod 600 .ssh/ansible
